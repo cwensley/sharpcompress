2b38f44ea6f7cdc6867084bf6fce67535dcfd9bf
Adam Hathcock <adam@hathcock.co.uk>
2011-04-21 10:18:19 +0100

    Adding more Opens to ArchiveFactory.cs

--- SharpCompress/Archive/ArchiveFactory.cs
+++ SharpCompress/Archive/ArchiveFactory.cs
@@ -7,15 +7,14 @@ using SharpCompress.Common;
 namespace SharpCompress.Archive
 {
     public class ArchiveFactory
-    {   /// <summary>
-        /// Opens a Reader for Non-seeking usage
+    {
+        /// <summary>
+        /// Opens an Archive for random access
         /// </summary>
         /// <param name="stream"></param>
-        /// <param name="listener"></param>
         /// <param name="options"></param>
         /// <returns></returns>
-        public static IArchive OpenArchive(Stream stream, IExtractionListener listener,
-            Options options = Options.KeepStreamsOpen)
+        public static IArchive Open(Stream stream, Options options = Options.KeepStreamsOpen)
         {
             stream.CheckNotNull("stream");
             if (!stream.CanRead || !stream.CanSeek)
@@ -37,33 +36,79 @@ namespace SharpCompress.Archive
             throw new InvalidOperationException("Cannot determine compressed stream type.");
         }
 
+#if !PORTABLE
         /// <summary>
-        /// Opens a CompressedStreamReader for Non-seeking usage
+        /// Constructor expects a filepath to an existing file.
         /// </summary>
-        /// <param name="stream"></param>
-        /// <param name="options"></param>
-        /// <returns></returns>
-        public static IArchive OpenArchive(Stream stream, Options options = Options.KeepStreamsOpen)
+        /// <param name="filePath"></param>
+        public static IArchive Open(string filePath)
         {
-            stream.CheckNotNull("stream");
-            return OpenArchive(stream, new NullExtractionListener(), options);
+            return Open(filePath, Options.None);
         }
 
-#if !PORTABLE
-        public static void ExtractToDirectory(string sourceArchive, string destinationDirectoryName)
+        /// <summary>
+        /// Constructor with a FileInfo object to an existing file.
+        /// </summary>
+        /// <param name="fileInfo"></param>
+        public static IArchive Open(FileInfo fileInfo)
         {
-            using (FileStream stream = File.OpenRead(sourceArchive))
+            return Open(fileInfo, Options.None);
+        }
+
+        /// <summary>
+        /// Constructor expects a filepath to an existing file.
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="options"></param>
+        public static IArchive Open(string filePath, Options options)
+        {
+            filePath.CheckNotNullOrEmpty("filePath");
+            return Open(new FileInfo(filePath), options);
+        }
+
+        /// <summary>
+        /// Constructor with a FileInfo object to an existing file.
+        /// </summary>
+        /// <param name="fileInfo"></param>
+        /// <param name="options"></param>
+        public static IArchive Open(FileInfo fileInfo, Options options)
+        {
+            fileInfo.CheckNotNull("fileInfo");
+            using (var stream = fileInfo.OpenRead())
             {
-                IArchive archive = OpenArchive(stream);
-                foreach (IArchiveEntry entry in archive.Entries)
+                if (ZipArchive.IsZipFile(stream))
                 {
-                    string path = Path.Combine(destinationDirectoryName, Path.GetFileName(entry.FilePath));
-                    using (FileStream output = File.OpenWrite(path))
-                    {
-                        entry.WriteTo(output);
-                    }
+                    return ZipArchive.Open(fileInfo, options);
                 }
+                stream.Seek(0, SeekOrigin.Begin);
+                if (RarArchive.IsRarFile(stream))
+                {
+                    return RarArchive.Open(fileInfo, options);
+                }
+                throw new InvalidOperationException("Cannot determine compressed stream type.");
             }
+        }
+
+        /// <summary>
+        /// Extract to specific directory, retaining filename
+        /// </summary>
+        public static void WriteToDirectory(string sourceArchive, string destinationDirectory, IExtractionListener listener,
+            ExtractOptions options = ExtractOptions.Overwrite)
+        {
+            IArchive archive = Open(sourceArchive);
+            foreach (IArchiveEntry entry in archive.Entries)
+            {
+                entry.WriteToDirectory(destinationDirectory, listener, options);
+            }
+        }
+
+        /// <summary>
+        /// Extract to specific directory, retaining filename
+        /// </summary>
+        public static void WriteToDirectory(string sourceArchive, string destinationPath,
+            ExtractOptions options = ExtractOptions.Overwrite)
+        {
+            WriteToDirectory(sourceArchive, destinationPath, new NullExtractionListener(), options);
         }
 #endif
     }
